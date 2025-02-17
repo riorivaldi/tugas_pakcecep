dfe243c13acdcfc3cd10ffedd096b47f0d609d09
fiqrizaelani <fiqrizaelani4@gmail.com>
2025-02-17 17:28:34 +0700

    Auth

--- src/app/services/api.tsx
+++ src/app/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://94da-160-19-227-39.ngrok-free.app/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://ec59-103-228-242-54.ngrok-free.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
